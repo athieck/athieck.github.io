9cbfcb90b2b143e08578530f00155ca52160a938
Andrew Thieck <athieck22@gmail.com>
2019-09-21 16:32:25 -0400

    WIP

--- generate.go
+++ generate.go
@@ -43,7 +43,7 @@ func main() {
 	cmd.Run()
 
 	// Crop the image
-	cmd = exec.Command("ffmpeg", "-i", "wallpaper_scaled.png", "-vf", "crop=150:150:50:176", "0_0.png")
+	cmd = exec.Command("ffmpeg", "-i", "wallpaper_scaled.png", "-vf", "crop=150:150:60:184", "0_0.png")
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	err := cmd.Run()
