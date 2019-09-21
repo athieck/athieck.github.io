ac189987cd3a93f97f85f9393548c3cf06e1a7df
Andrew Thieck <athieck22@gmail.com>
2019-09-21 11:57:22 -0400

    again

--- generate.go
+++ generate.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("ffmpeg", "-i", "wallpaper2.png", "-vf", "crop=150:150:50:100", "0_0.png")
+	cmd := exec.Command("rm", "0_0.png")
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("could not remove\n")
+		return
+	}
+	cmd = exec.Command("ffmpeg", "-i", "wallpaper2.png", "-vf", "crop=150:150:160:240", "0_0.png")
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	err := cmd.Run()
